00748ad58b1dced5776e7ee78bd1c60028ac3f54
Vladimir Scherbina <vns.scherbina@gmail.com>
2022-03-29 21:55:22 +0300

    feat: add docker support

--- index.py
+++ index.py
@@ -88,5 +88,5 @@ async def read_user(username: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("index:app", host="127.0.0.1",
+    uvicorn.run("index:app", host="0.0.0.0",
                 port=8000, reload=True, ws="websockets", log_level="info")
